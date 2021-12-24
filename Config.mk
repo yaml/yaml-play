678b0833467d1b8fd91f68f89c6edbb6dc4a821e
Ingy döt Net <ingy@ingy.net>
2021-12-23 21:04:31 -0500

    Publish

--- Config.mk
+++ Config.mk
@@ -1,3 +1,11 @@
-RUNTIMES_VERSION := 0.1.6
+CONFIG_MK := $(ROOT)/../yaml-test-runtimes/Config.mk
+
+ifneq (,$(wildcard $(CONFIG_MK)))
+    include $(CONFIG_MK)
+    RUNTIMES_VERSION := $(TAG_MAIN)
+endif
+ifneq (,$(RUNTIMES_VERSION))
+    export RUNTIMES_VERSION
+endif
 
 SANDBOX_VERSION := $(RUNTIMES_VERSION)
